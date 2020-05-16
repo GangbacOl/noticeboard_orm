853b970c84a2ac73ed5b11434066fa223c6e6931
GangbacOl <gangyeonjo336@gmail.com>
2020-05-17 01:43:59 +0900

    URL을 '/user'에서 '/auth'로 변경.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use('/', require('./routes/main/index'));
-app.use('/user', require('./routes/user/index'));
+app.use('/auth', require('./routes/auth/index'));
 app.use('/post', require('./routes/post/index'));
 
 app.set('jwt-secret', config.secret);
